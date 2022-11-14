a7af609ea91be6206c36893a6966c83f584a3c43
unknown <vshs.harisurya@gmail.com>
2022-11-14 09:42:25 +0530

    controller fix

--- src/main/java/com/hotel/booking/controller/LoginController.java
+++ src/main/java/com/hotel/booking/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.hotel.booking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "app/")
+@RestController
+@RequestMapping(value = "app/v1/")
 public class LoginController {
 
     @GetMapping(name = "test")
