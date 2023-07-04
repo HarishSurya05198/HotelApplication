ddffb343de51366c2d9dc6fe7875b9831a02cddd
unknown <vshs.harisurya@gmail.com>
2023-07-04 11:45:59 +0530

    update password and disable sql show

--- src/main/java/com/hotel/booking/HotelBookingApplication.java
+++ src/main/java/com/hotel/booking/HotelBookingApplication.java
@@ -9,7 +9,7 @@ import com.twilio.Twilio;
 public class HotelBookingApplication {
 	
 	public final static String ACCOUNT_ID = "AC0897ff7987f0f4c891968966cc393b61";
-	public final static String AUTH_TOKEN = "659e93845f9dc903ba794bc126a06ff5";
+	public final static String AUTH_TOKEN = "bd6e7d282de431a4f12e145658f3abe1";
 	
 	public static void main(String[] args) {
 		Twilio.init(ACCOUNT_ID, AUTH_TOKEN);
